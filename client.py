4e38f6334e13863d6593699c8d159a608994b12a
nhattran123 <crimeson81@gmail.com>
2021-02-11 22:21:44 +0000

    Initial commit

---
+++ client.py
@@ -0,0 +1,13 @@
+
+key = ["hi", "how are you", "what is c2c?", "program", "!help", "!c2c", "!greetings"]
+
+Responses = {
+"hi"  : "Hello!", 
+"how are you" : "I am fine, thank you!",
+"what is c2c?" : "C2C is a non-profit organization that aim toward dramatically increase the number of minority and low-income high school students who enter and excel in STEM undergraduate majors and careers. ",
+"program": "In Code 2 College. we offered a lot of programs such as Turbo ",
+"!help" : "Here is a short list of command you can use: !greetings, !c2c",
+"!c2c"  : "You can ask info about C2C by typing 'program' or 'what is c2c?'", 
+"!greetings": "You can type 'hi' or 'how are you' to greet me. I would appreciate it :)" 
+}
+
---
+++ main.py
@@ -0,0 +1,29 @@
+#List to do for this project:
+#1. Try to make the chatbot more complex
+#2. Add new comand, such as !help for lists for instruction a user can type to use the Bot.(Done)
+#3. If possible, create a HTML page for my Bot.
+#4. Add some stuff just for fun (Put something here if you have an idea: )
+
+from client import key, Responses
+
+#Input names 
+user_name = str(input("Welcome to C2C Chat Bot system, may I have your first name? "))
+chatbot_name  = "C2D2"
+
+print(f"{chatbot_name}: Hi {user_name}, my name is {chatbot_name}, C2C's chatbot. If you confuse of what to ask me, you can type !help  for a list of prompt you can use. If you want to end the chat session, just simply type quit! ")
+
+#The key to end the chat bot
+quit = "quit"
+
+#Algorithm that promt the user a question so that my bot can answer. 
+user_input = str.lower(input(f"{user_name}: "))
+
+while user_input != quit:
+  
+  if user_input in key:
+    print(chatbot_name + ": " + Responses[user_input])
+  
+  else:
+    print (f"{chatbot_name}: Sorry, I don't understand that")
+  
+  user_input = str.lower(input(f"{user_name}: "))
